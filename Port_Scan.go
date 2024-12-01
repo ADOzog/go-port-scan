bdd9551c0acf90dd1206ecd9175a86c8dd9f6a28
ADOzog <ozoganthony@gmail.com>
2024-12-01 16:51:55 -0500

    added scan all function

--- Port_Scan.go
+++ Port_Scan.go
@@ -1,13 +1,25 @@
 package main
-import ("net"; "fmt"; "flag"; "strconv"; "strings"; "time")
+import ("net"; "fmt"; "flag"; "strconv"; "strings"; "time"; "sync")
 
 
 // this will need to run multiple "Dial" functions for each port
-func Port_Scan (network_connection string, IP_ports string, timeout_duration time.Duration) (net.Conn,error){
+func Port_Scan (network_connection string, IP_ports string, timeout_duration time.Duration, wg *sync.WaitGroup) {
   // IP ports will be an array of strings in the future 
-  
+  defer wg.Done() 
   conn , err := net.DialTimeout(network_connection , IP_ports, timeout_duration)
-  return conn , err
+    _ = err
+    // the error handleing got a bit complex but in the future the goal is to make i/o timeout go away or be made simple
+    // if err != nil {
+    // if netErr , ok := err.(net.Error); {
+    //     fmt.Println("i/o")
+    //   }else{    
+    //     fmt.Println("You had an error")
+    //     fmt.Println(err)
+    //   }
+    // }
+    if conn != nil {
+      fmt.Println("Successfully connected to:", conn.RemoteAddr())
+    }
 
 
 }
@@ -32,11 +44,13 @@ func main (){
   var multiple_ports_array []int
   var timeout_int int
   // var set_of_ports [...]int
+  
 
   var IP string
   var network_connection string 
   // var IP_port_single string 
-
+  
+  var wg sync.WaitGroup
 
   network_connection = "tcp"
   
@@ -48,12 +62,26 @@ func main (){
   flag.StringVar(&multiple_ports_string_form, "Sm", "", "Sm is used to scan multiple ports, just write each port you would like scan seperated by spaces and surrounded by '' ")
 
   flag.IntVar(&timeout_int, "T", 5, "T is used to set the amout of time till a port is considered failed to connect")
+
+
+  Scan_all := flag.Bool( "Sa", false, "Sa is used to scan all ports from 1 to 1024")
+
   // Make sure no flags appear after this line######################################################################### 
   //###################################################################################################################
   flag.Parse()
 
 
   timeout_duration := time.Duration(timeout_int) * time.Second
+
+
+  // doing this with slices may impact the speed I will look at this in the future
+  if *Scan_all {
+    for i := range(1024){
+      multiple_ports_array = append(multiple_ports_array,i)
+    }
+  }
+
+
 
   multiple_ports_array_of_strings := strings.Fields(multiple_ports_string_form)
   for _,port_string := range multiple_ports_array_of_strings{
@@ -66,30 +94,17 @@ func main (){
   }
   // this is necessary because of how the "Dial" function works
   // in the future this will need to be its own function that uses a for loop to cover all the ports
-  // if single_port != -1{
-    // string_single_port := strconv.Itoa(single_port)
-    // IP_port_single = Ip + ":" + string_single_port
-//    fmt.Println("IP_port_single was declared")
-//    fmt.Println(IP_port_single)
-  // }else {
-    // IP_port_single = ""
-    // _ = IP_port_single 
-
-  // }
-  
+   
   Array_IP_ports := IP_port_maker(multiple_ports_array , IP)
-
+  
   for _,IP_port := range Array_IP_ports{ 
-  conn , err := Port_Scan( network_connection, IP_port, timeout_duration)
-  if err != nil {
-    fmt.Println("You had an error")
-    fmt.Println(err)
+    wg.Add(1)
+    go Port_Scan( network_connection, IP_port, timeout_duration, &wg)
+
+
   }
-  if conn != nil {
-    fmt.Println("Successfully connected to:", conn.RemoteAddr())
-  }
-}
-  // fmt.Println(multiple_ports_array)
+  wg.Wait()
+ 
 
 
 
